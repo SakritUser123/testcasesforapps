1d22fa031a2530292009b7a57bfb7b724605fad2
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-04-01 21:46:51 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def chat_stream(prompt):
+    response = f'You said, "{prompt}" ...interesting.'
+
+
+    for char in response:
+        yield char
+        time.sleep(.02)
+
+  chat_stream()
